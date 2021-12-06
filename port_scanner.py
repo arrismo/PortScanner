5296066fdaedba13f5746644f903e6894cad3016
Renee Wu <43734607+rwu08@users.noreply.github.com>
2021-12-05 20:30:05 -0500

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,26 +1,33 @@
-
+import time
+import scapy
+import socket
 def normalScan(ip, numOfPorts, order):
   if order == "inOrder":
     print("Starting port scan")
-    print("Interesting ports on:"+ip)
+    if numOfPorts==65536:
+          print("Interesting ports on:"+ip)
+    else:
+      print("All ports on: "+ip)
     print("PORT\tSTATE\tSERVICE")
     for port in range(1, numOfPorts):
-          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-          socket.setdefaulttimeout(1)
-          result = s.connect_ex((ip, port))
-          if numOfPorts == 65536:
-            if result == 0:
-              portNum = sock.getsockname()
-              state = "Open"
-              service = socket.getservbyport(portNum, tcp)
-              print(portNum+"\t"+state+"\t"+service)
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      socket.setdefaulttimeout(1)
+      result = s.connect_ex((ip, port))
+      if result == 0:
+        portNum = sock.getsockname()
+        state = "Open"
+        service = socket.getservbyport(portNum, tcp)
+        print(portNum+"\t"+state+"\t"+service)
 
       # create socket and get port #, state, and service
       # List of interesting ports
       # print
   else:
     print("Starting port scan")
-    print("All ports on:"+ip)
+    if numOfPorts==65536:
+          print("Interesting ports on:"+ip)
+    else:
+      print("All ports on: "+ip)
     print("PORT\tSTATE\tSERVICE")
     r = list(range(portNum))
     random.shuffle(r)
@@ -39,48 +46,48 @@ def normalScan(ip, numOfPorts, order):
     # same as above
 
 
-def tcpSYN(numOfPorts, order):
-   if order == inOrder:
-    for i in range(numOfPorts):
+#def tcpSYN(numOfPorts, order):
+#  if order == inOrder:
+    #for i in range(numOfPorts):
       # create socket
       # create SYN Packet and send
-      if flag == synack:
+     # if flag == synack:
         # get port #, state, and service and print
       # print
       # create
       # send RST Packet
-  else:  
+ # else:  
     # randomize range(numOfPorts)
     # same as above   
 
-def tcpFIN(numOfPorts,order):
+#def tcpFIN(numOfPorts,order):
  
-  if order==inOrder:
-    for i in range(numOfPorts):
+  #if order==inOrder:
+   # for i in range(numOfPorts):
       # List of interesting ports
 
       # create socket
       # create TCP Packet and send 
-      if open: 
+     # if open: 
         # send packet 
-      if closed: 
+      #if closed: 
         # exit 
         # print something 
-  else:  
+  #  else:  
     # randomize range(numOfPorts)
     # List of all interesting ports
 
     # same as above   
 
 
-def printTable(mode,numOfPorts,order):
+def printTable(ip,mode,numOfPorts,order):
   # numOfPorts
-  if normal:
-    normalScan(numOfPorts,order)
-  elif syn:
-    tcpSYN(numOfPorts,order)
+  if mode=="normal":
+    normalScan(ip,numOfPorts,order)
+  elif mode=="syn":
+    tcpSYN(ip,numOfPorts,order)
   else:
-    tcpFin(numOfPorts,order)
+    tcpFin(ip,numOfPorts,order)
 
 
 def checkIP(IP):
@@ -90,15 +97,20 @@ def checkIP(IP):
 
 
 def scanIP():
-  if checkIP(IP):
+  #127.0.0.1if checkIP(IP):
+    ip=input("ip: ")
+    mode=input("mode: ")
+    order=input("order: ")
+    numOfPorts=100
+
+    start=time.time()
     
-    start=#start time
-    closedPorts=numOfPorts-len(get_open_ports(IP))
+    closedPorts=numOfPorts-len(get_open_ports(ip))
     print("There are "+closedPorts+" closed ports.")
 
     printTable(mode, numOfPorts,order)
-    end=#end time 
-    print("Finished scanning IP: "IP+" in "+(end-start)+" seconds.")
+    end=time.time()
+    print("Finished scanning IP: "+ip+" in "+(end-start)+" seconds.")
   
 
 def main():
