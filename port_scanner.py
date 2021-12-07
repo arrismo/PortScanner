60fd69354191325dde0b3705d56783dc8d5f435c
Arris Moise <mikaelmoise00@gmail.com>
2021-12-06 22:04:44 -0500

    merging everything together

--- port_scanner.py
+++ port_scanner.py
@@ -62,39 +62,85 @@ def normalScan(ip, numOfPorts, order):
 
 
 def tcpSYN(ip, numOfPorts, order):
-       # if order==inOrder:
-  for i in range(len(numOfPorts)):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # server.connect((target, i))
-    server.bind(('', i))  # socket is reachable by any machine
-    SYNPacket = IP(dst=ip)/TCP(dport=80, flags="S")
-    SYNresponse = sr1(SYNPacket)
-    # will output SA or RA  SA = SYnack
-    flag = SYNresponse.sprintf('%TCP.flags%')
-    if flag == 'SA':
-      print("FOUND SYNACK")
-      # create RST packet
-      RSTpacket = IP(dst=ip)/TCP(dport=80, flags="R")
-      send(RSTpacket)
+    if order == "inOrder":
+      print("Starting port scan")
+      if numOfPorts == 65536:
+          print("All ports on:"+ip)
+      else:
+        print("Interesting ports on: "+ip)
+      print("PORT\tSTATE\tSERVICE")
+
+      for i in range(numOfPorts):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('', i))
+        SYNPacket = IP(dst=ip)/TCP(dport= i, flags="S")
+        SYNresponse = sr1(SYNPacket,verbose=0) # will output SA or RA  SA = SYnack
+        flag = SYNresponse.sprintf('%TCP.flags%')
+        if flag == 'SA':
+          #print("FOUND SYNACK")
+          # create RST packet
+          RSTpacket = IP(dst=ip)/TCP(dport= i , flags="R")
+          send(RSTpacket, verbose=False)
+        else:
+            sys.exit()
+
     else:
-      print("DID NOT FIND SYNACK")
-           # randomize range(numOfPorts)
+        print("Starting port scan")
+        if numOfPorts == 65536:
+            print("All ports on:"+ip)
+        else:
+          print("Interesting ports on: "+ip)
+        print("PORT\tSTATE\tSERVICE")
+
+        r = list(range(numOfPorts))
+        random.shuffle(r)
+        for i in r:
+          server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          server.bind(('', i))
+          SYNPacket = IP(dst=ip)/TCP(dport= i, flags="S")
+          SYNresponse = sr1(SYNPacket,verbose=0) # will output SA or RA  SA = SYnack
+          flag = SYNresponse.sprintf('%TCP.flags%')
+          if flag == 'SA':
+            #print("FOUND SYNACK")
+            # create RST packet
+            RSTpacket = IP(dst=ip)/TCP(dport= i , flags="R")
+            send(RSTpacket, verbose=False)
+          else:
+              sys.exit()
+
+
+      # state = "Open"
+      # r = list(range(numOfPorts))
+      # for i in range(numOfPorts):
+      #   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      #   server.bind(('', i))  # socket is reachable by any machine
+      #   SYNPacket = IP(dst=ip)/TCP(dport= i, flags="S")
+      #   SYNresponse = sr1(SYNPacket,verbose=0)
+      #   service = socket.getservbyport(portNum, "tcp")
+      #   print(portNum)
+      #   print(state)
+      #   print(service)
 
 
 def tcpFIN(ip, numOfPorts, order):
-      # if order==inOrder:
-  for i in range(len(numOfPorts)):
-      # List of interesting ports
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('', i))
-    FINPacket = IP(dst=ip)/TCP(dport=80, flags="F")
-    FINresponse = sr1(FINPacket)
-    if FINresponse == None:
-      print("Port is open")
-      send(FINPacket)
+    print("Starting port scan")
+    if numOfPorts == 65536:
+        print("All ports on:"+ip)
     else:
-      print("Port is closed")
-      sys.exit(1)
+        print("Interesting ports on: "+ip)
+    print("PORT\tSTATE\tSERVICE")
+
+    for i in range(numOfPorts):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind(('', i))
+        FINPacket = IP(dst=ip)/TCP(dport= i , flags="F")
+        FINresponse = sr1(FINPacket,verbose=0)
+        if FINresponse == None:
+            #print("Port is open")
+            send(FINPacket,verbose=False)
+        else:
+            #print("Port is closed")
+            sys.exit(1)
 
 
 def printTable(ip, mode, numOfPorts, order):
@@ -104,7 +150,7 @@ def printTable(ip, mode, numOfPorts, order):
   elif mode == "syn":
     tcpSYN(ip, numOfPorts, order)
   else:
-    tcpFin(ip, numOfPorts, order)
+    tcpFIN(ip, numOfPorts, order)
 
 
 def checkIP(IP):
@@ -123,20 +169,21 @@ def checkIP(IP):
 def scanIP():
   # 127.0.0.1if checkIP(IP):
   ip="131.229.72.13"
-  mode="normal"
+  mode="SYN"
   order="dssdfds"
-  numOfPorts=100
+  numOfPorts=1
 
   start=time.time()
-    
+
   # closedPorts=numOfPorts-len(get_open_ports(ip))
   # print("There are "+closedPorts+" closed ports.")
 
   printTable(ip, mode, numOfPorts,order)
+  print("done")
   end=time.time()
 
   # print("Finished scanning IP: "+ip+" in "+(end-start)+" seconds.")
-  
+
 
 def main():
   # parameters mode, how many ports, order
